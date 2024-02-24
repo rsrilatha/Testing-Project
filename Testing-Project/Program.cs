0b139f9eed963c21a3fafb1ebcb25fe2e5f77a9d
rsrilatha <rsrilatha2510@gmail.com>
2024-02-25 10:44:24 +1100

    initial code changes for turnup portal automation

---
+++ Testing-Project/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver ();
+driver.Manage().Window.Maximize();
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButton.Click();
+IWebElement hellohari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/ul/li[2]/a"));
+if (hellohari.Text == "hello hari")
+{
+    Console.WriteLine("user has logged in succesfully");
+        }
+else
+{
+    Console.WriteLine("user not logged in successfully");
+
+}
+
